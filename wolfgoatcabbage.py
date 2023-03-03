64479127689f6c6fc75f1a1a5c1529effc6f7a16
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-03-03 12:15:57 -0800

    fixed constructore and goal state, removed result and action

--- wolfgoatcabbage.py
+++ wolfgoatcabbage.py
@@ -1,7 +1,7 @@
 from search import *
 
 class WolfGoatCabbage(Problem):
-    def __init__(self, initial=({'F', 'W', 'G', 'C'}, {}), goal=({}, {'F', 'W', 'G', 'C'})):
+    def __init__(self, initial={'F', 'W', 'G', 'C'}, goal={}):
         super().__init__(initial, goal)
 
     def goal_test(self, state):
@@ -11,46 +11,25 @@ class WolfGoatCabbage(Problem):
     def result(self, state, action):
         """returns the new state reached from the given state
         and the given action. Assume that the action is valid."""
-        if 'F' in state[0]:
-            for item in action:
-                state[0].remove(item)
-                state[1].add(item)
-        else:
-            for item in action:
-                state[1].remove(item)
-                state[0].add(item)
+        return 'to be implemented'
 
     def actions(self, state):
         """returns a list of valid actions in the given state"""
-
-        def is_restricted(state):
-            """returns True if the given state is restricted"""
-            restrictions = [{'W', 'G'}, {'G', 'C'}]
-            for restriction in restrictions:
-                if restriction.issubset(state):
-                    return True
-            return False
-
-        actions = []
-        bank = state[0] if 'F' in state[0] else state[1]
-
-        # can farmer return alone?
-        bank = bank.copy()
-        bank.remove('F')
-        if not is_restricted(bank):
-            actions.append({'F'})
-
-        # can farmer return with one item?
-        for item in bank:
-            items = bank.copy()
-            items.remove(item)
-            if not is_restricted(items):
-                actions.append({'F', item})
+        return "to be implemented"
 
 
 if __name__ == '__main__':
     wgc = WolfGoatCabbage()
-    solution = depth_first_graph_search(wgc).solution()
-    print(solution)
-    solution = breadth_first_graph_search(wgc).solution()
-    print(solution)
+    goal_test = wgc.goal_test(set())
+    print('goal_test: ', goal_test)
+
+    result = wgc.result(set({'F', 'W', 'G', 'C'}), set({'F', 'G'}))
+    print('result: ', result)
+
+    actions = wgc.actions(set({'F', 'W', 'G', 'C'}))
+    print('actions: ', actions)
+
+    # solution = depth_first_graph_search(wgc).solution()
+    # print(solution)
+    # solution = breadth_first_graph_search(wgc).solution()
+    # print(solution)
