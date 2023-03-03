169fbf810968280be8ba60b8029c44c9d177e9f2
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-03-03 12:29:27 -0800

    fixed constructor default initial and goal from dict to set

--- wolfgoatcabbage.py
+++ wolfgoatcabbage.py
@@ -1,7 +1,7 @@
 from search import *
 
 class WolfGoatCabbage(Problem):
-    def __init__(self, initial={'F', 'W', 'G', 'C'}, goal={}):
+    def __init__(self, initial=set({'F', 'W', 'G', 'C'}), goal=set()):
         super().__init__(initial, goal)
 
     def goal_test(self, state):
@@ -11,10 +11,25 @@ class WolfGoatCabbage(Problem):
     def result(self, state, action):
         """returns the new state reached from the given state
         and the given action. Assume that the action is valid."""
-        return 'to be implemented'
+
+        return state - action
 
     def actions(self, state):
         """returns a list of valid actions in the given state"""
+
+        def is_restricted(state):
+            restrictions = [{'W', 'G'}, {'G', 'C'}]
+            for restriction in restrictions:
+                if restriction.issubset(state):
+                    return True
+            return False
+
+        bank = state.copy()
+
+
+        if not is_restricted(bank):
+            return [bank]
+
         return "to be implemented"
 
 
@@ -24,7 +39,7 @@ if __name__ == '__main__':
     print('goal_test: ', goal_test)
 
     result = wgc.result(set({'F', 'W', 'G', 'C'}), set({'F', 'G'}))
-    print('result: ', result)
+    print('new state: ', result)
 
     actions = wgc.actions(set({'F', 'W', 'G', 'C'}))
     print('actions: ', actions)
