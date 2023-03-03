b717773d043c40d8427bef90102ce8f75a7e5b7e
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-03-03 12:58:27 -0800

    changed sets to frozen set to fix set not subscriptable error

--- wolfgoatcabbage.py
+++ wolfgoatcabbage.py
@@ -1,7 +1,7 @@
 from search import *
 
 class WolfGoatCabbage(Problem):
-    def __init__(self, initial=set({'F', 'W', 'G', 'C'}), goal=set()):
+    def __init__(self, initial=frozenset({'F', 'W', 'G', 'C'}), goal=frozenset()):
         super().__init__(initial, goal)
 
     def goal_test(self, state):
@@ -20,7 +20,7 @@ class WolfGoatCabbage(Problem):
         """returns a list of valid actions in the given state"""
 
         def is_restricted(state):
-            restrictions = [set({'W', 'G'}), set({'G', 'C'})]
+            restrictions = [frozenset({'W', 'G'}), frozenset({'G', 'C'})]
             for restriction in restrictions:
                 if restriction.issubset(state):
                     return True
@@ -29,34 +29,34 @@ class WolfGoatCabbage(Problem):
         actions = []
         # can farmer leave alone?
         bank = state.copy()
-        bank = bank - set({'F'})
+        bank = bank - frozenset({'F'})
         if not is_restricted(bank):
-            actions.append(set({'F'}))
+            actions.append(frozenset({'F'}))
 
         # can farmer take one item?
         for item in bank:
-           items = bank.copy() - set({item})
+           items = bank.copy() - frozenset({item})
            if not is_restricted(items):
-               actions.append(set({'F', item}))
+               actions.append(frozenset({'F', item}))
 
         return actions
 
 
 if __name__ == '__main__':
     wgc = WolfGoatCabbage()
-    goal_test = wgc.goal_test(set())
+    goal_test = wgc.goal_test(frozenset())
     print('goal_test: ', goal_test)
 
-    # result = wgc.result(set({'F', 'W', 'G', 'C'}), set({'F', 'G'}))
+    # result = wgc.result(frozenset({'F', 'W', 'G', 'C'}), frozenset({'F', 'G'}))
     # print('new state: ', result)
 
-    result = wgc.result(set({'W'}), set({'F', 'C'}))
+    result = wgc.result(frozenset({'W'}), frozenset({'F', 'C'}))
     print('new state: ', result)
 
-    actions = wgc.actions(set({'F','G', 'C'}))
+    actions = wgc.actions(frozenset({'F','G', 'C'}))
     print('actions: ', actions)
 
-    # solution = depth_first_graph_search(wgc).solution()
-    # print(solution)
-    # solution = breadth_first_graph_search(wgc).solution()
-    # print(solution)
+    solution = depth_first_graph_search(wgc).solution()
+    print(solution)
+    solution = breadth_first_graph_search(wgc).solution()
+    print(solution)
